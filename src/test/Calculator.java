6c6d8146ad5f0ece2445f79e2507d5303f4f0a7f
HarshalGourkar <harshalgourkar1804@gmail.com>
2024-09-14 21:56:31 +0530

    first commit

---
+++ src/test/Calculator.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Calculator {
+	
+	private String n;
+	
+	public Calculator(String n ) {
+		this.n=n;
+	}
+
+	public String getN() {
+		return n;
+	}
+
+	public void setN(String n) {
+		this.n = n;
+	}
+}
